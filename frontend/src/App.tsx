b6b867c0925a5def800be84c3233d0bc4d47ec42
Jaciane Cruz <jaciane.cruz@ccc.ufcg.edu.br>
2021-05-06 00:21:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { DataTable } from 'components/DataTable';
+import { Footer } from 'components/Footer';
+import { NavBar } from 'components/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <div className="text-primary">
-      <h1>hello world</h1>
-    </div>
+    <> 
+      <NavBar></NavBar>
+      <div className="container">
+      <h1 className="text-primary">hello world</h1>
+      <DataTable></DataTable>
+      </div>
+      <Footer></Footer>
+    </>
+
   );
 }
 
