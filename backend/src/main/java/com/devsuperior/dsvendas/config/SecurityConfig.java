45887f733bb53a324801e7d88509b4cf11dfff50
Jaciane Cruz <jaciane.cruz@ccc.ufcg.edu.br>
2021-05-06 23:38:47 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsvendas.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
